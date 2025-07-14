a6f607365f4c8b6322bf525a4251a3543014fcd2
Dorian Dishani <dishani@macbookair.myfiosgateway.com>
2025-07-14 18:18:59 -0400

    Update tax route to support path param

--- routes/taxRoute.js
+++ routes/taxRoute.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getOpaDetails } = require('../services/opaScraper');
 
-router.get('/', async (req, res) => {
-  const account = req.query.account;
+router.get('/:account', async (req, res) => {
+  const account = req.params.account;
   if (!account) return res.status(400).json({ error: 'Missing account number' });
 
   try {
